138d3d1f85b11e5c5a24536edc85bdbd1d0434f5
YarynaFialko <yarynafialko@gmail.com>
2024-01-17 23:28:12 +0200

    Added Delivery Manager UI

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour {
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
 
     public static DeliveryManager Instance { get; private set; }
     [SerializeField] private RecipeListSO recipeListSO;
@@ -14,7 +18,7 @@ public class DeliveryManager : MonoBehaviour {
 
     private void Awake() {
         Instance = this;
-        
+
         waitingRecipeSOList = new List<RecipeSO>();
     }
 
@@ -24,9 +28,10 @@ public class DeliveryManager : MonoBehaviour {
             spawnRecipeTimer = spawnRecipeTimerMax;
             
             if (waitingRecipeSOList.Count < waitingRecipesMax) {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log("Waiting recipe: " + waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
                 waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -52,14 +57,17 @@ public class DeliveryManager : MonoBehaviour {
                 }
 
                 if (plateContentsMatchesRecipe) {
-                    Debug.Log("Recipe delivered: " + waitingRecipeSO.recipeName);
                     waitingRecipeSOList.Remove(waitingRecipeSO);
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
         }
+    }
 
-        Debug.Log("Recipe not found");
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
     }
 
 
