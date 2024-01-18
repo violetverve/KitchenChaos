c2455ed8d7774bcede15698c44c781979f4ad99a
YarynaFialko <yarynafialko@gmail.com>
2024-01-18 22:28:12 +0200

    Added Options UI, Audio Levels

--- Assets/Scripts/UI/GamePausedUI.cs
+++ Assets/Scripts/UI/GamePausedUI.cs
@@ -7,6 +7,7 @@ public class GamePausedUI : MonoBehaviour {
 
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button optionsButton;
 
     private void Awake() {
         resumeButton.onClick.AddListener(() => {
@@ -14,6 +15,9 @@ public class GamePausedUI : MonoBehaviour {
         });
         mainMenuButton.onClick.AddListener(() => {
             Loader.LoadScene(Loader.Scene.MainMenuScene);
+        });
+        optionsButton.onClick.AddListener(() => {
+            OptionsUI.Instance.Show();
         });
     }
 
