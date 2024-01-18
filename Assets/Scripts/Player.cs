a26ebd4219c4e4ab6f92c59c947c0e6267383034
YarynaFialko <yarynafialko@gmail.com>
2024-01-18 15:43:22 +0200

    Added Game Start Countdown

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,12 +38,16 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+        
         if (selectedCounter != null) {
             selectedCounter.Interact(this);
         }
     }
 
     private void GameInput_OnInteractAlternateAction(object sender, System.EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null) {
             selectedCounter.InteractAlternate(this);
         }
