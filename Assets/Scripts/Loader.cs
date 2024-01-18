db2b5ba92029e26e9f306d194fc206d48d65d90c
YarynaFialko <yarynafialko@gmail.com>
2024-01-18 20:11:17 +0200

    Added Loading Scene , Main Menu Visuals

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,23 @@
+using UnityEngine.SceneManagement;
+
+public static class Loader {
+
+    public enum Scene {
+        MainMenuScene,
+        LoadingScene,
+        GameScene
+    }
+
+    private static Scene targetScene;
+
+    public static void LoadScene(Scene targetScene) {
+        Loader.targetScene = targetScene;
+   
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());    
+    }
+
+    public static void LoaderCallback() {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+
+}
